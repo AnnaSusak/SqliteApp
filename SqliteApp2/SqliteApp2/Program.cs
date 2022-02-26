c2d69c8f9f8d247e01c17880bf032b099ea06e07
AnnaSusak <70056031+AnnaSusak@users.noreply.github.com>
2022-02-26 17:00:57 +0300

    Add files via upload

---
+++ SqliteApp2/SqliteApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
+using SqliteApp2.Classes;
+
+namespace SqliteApp2
+{
+    class Program
+    {
+        static SqliteConnection connection = new SqliteConnection("Data Source=game.db");
+        static void Main(string[] args)
+        {
+            connection.Open();
+            SqlExpressionsMaker.Connection = connection;
+            while (true)
+            {
+                Menu.ShowVariants();
+                string sql_exp = SqlExpressionsMaker.MakeSqlExp(Menu.ReturnChosenVar(Menu.InputRequest()));
+                if (sql_exp != "")
+                {
+                    try
+                    {
+                        SqliteCommand command = new SqliteCommand(sql_exp, connection);
+                        try{
+                            Console.WriteLine(command.ExecuteNonQuery().ToString());
+                            Console.WriteLine("Succes");
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.ToString());
+                        }
+
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
+                    }
+                }
+            }
+        }
+        
+    }
+
+    
+}
